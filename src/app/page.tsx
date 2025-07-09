d37c18e9d6a14517ac9fc2bf3b3ec6d80ee0ac13
andesbit <adocarpel@gmail.com>
2025-07-09 08:17:25 -0500

    no se había actualizado las rutas automáticamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import {Bolo} from "../k/bolo";
-import {DisplayValueEnv} from "../k/env";
+import {Bolo} from "./k/bolo";
+import {DisplayValueEnv} from "./k/env";
 export default function Home() {
   return (
     <div className="bg-blue grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
